5240931e01c3bf4dc22286d305b4a6ca1ab2cddb
TheBestLife <632318927@qq.com>
2018-08-17 09:11:17 +0800

    2018/8/17 打卡 Ftp上传

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,9 +1,12 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
 import com.mmall.service.IFileService;
 
+import com.mmall.util.FTPUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -15,9 +18,11 @@ import java.util.UUID;
  * @author
  * @date
  */
+@Service("iFileService")
 public class FileServiceImpl implements IFileService {
    private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
 
+   @Override
    public String upload(MultipartFile file, String path) {
        //获取上传文件的名称
        String fileName = file.getOriginalFilename();
@@ -37,7 +42,10 @@ public class FileServiceImpl implements IFileService {
          file.transferTo(targetFile);
          //文件上传成功
 
-           // todo 将文件上传的ftp服务器
+           FTPUtil.uploadFile(Lists.newArrayList(targetFile));
+
+          //删除目录中的文件
+           targetFile.delete();
 
        }catch (IOException e){
            logger.error("上传文件异常",e);
